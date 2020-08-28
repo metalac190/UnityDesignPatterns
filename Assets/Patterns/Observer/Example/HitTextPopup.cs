be7ca9c25fb6aef9cf6a5d4d8525d4bf3232a2fc
Adam <achaysmail@gmail.com>
2020-08-28 08:00:17 -0500

    Observer - HitSFX
    Added hit sounds to the damaged/killed events to further reinforce the concept

---
+++ Assets/Patterns/Observer/Example/HitTextPopup.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This script uses the Observer pattern to start watching a
+/// Health object. This example shows how you can subscribe/unsubscribe
+/// whenever you need it, without using OnEnable or OnDisable.
+/// Technically we start observing in Awake, but we can call the
+/// 'StartObservingHealth' method whenever we want. Think of it as
+/// 'AcquireTarget' and 'LoseTarget' if we wanted to put it in a GameContext
+/// </summary>
+namespace Examples.Observer
+{
+    public class HitTextPopup : MonoBehaviour
+    {
+        [SerializeField] Health _healthToObserve = null;
+        [SerializeField] Text _textPopupUI = null;
+        [SerializeField] string _hitText = "Hit!";
+        [SerializeField] float _textPopupDuration = 1;
+
+        Health _observedHealth = null;
+        Coroutine _popupRoutine = null;
+
+        private void Awake()
+        {
+            StartObservingHealth(_healthToObserve);
+        }
+
+        public void StartObservingHealth(Health newHealthToObserver)
+        {
+            _observedHealth = newHealthToObserver;
+            // notify when target is damaged
+            _observedHealth.Damaged += OnObservedHealthDamaged;
+            _observedHealth.Killed += OnObservedHealthKilled;
+        }
+
+        public void StopObservingHealth()
+        {
+            // no longer watch target
+            _observedHealth.Damaged -= OnObservedHealthDamaged;
+            _observedHealth.Killed -= OnObservedHealthKilled;
+
+            _observedHealth = null;
+        }
+
+        void OnObservedHealthDamaged(int damaged)
+        {
+            string hitText = _hitText + " " + damaged.ToString();
+
+            if (_popupRoutine != null)
+                StopCoroutine(_popupRoutine);
+            _popupRoutine = StartCoroutine(TextPopup(hitText));
+        }
+
+        IEnumerator TextPopup(string textToShow)
+        {
+            _textPopupUI.text = textToShow;
+            yield return new WaitForSeconds(_textPopupDuration);
+            _textPopupUI.text = string.Empty;
+        }
+
+        void OnObservedHealthKilled()
+        {
+            StopObservingHealth();
+        }
+    }
+}
+
---
+++ Assets/Patterns/Observer/Example/PlaySoundOnDamaged.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script uses the observer pattern to play sound effects
+/// whenever the observed health takes damage or is killed.
+/// Note that this script doesn't know about any other scripts
+/// or Observers other than the Health, our Subject.
+/// </summary>
+namespace Examples.Observer
+{
+    [RequireComponent(typeof(Health))]
+    public class PlaySoundOnDamaged : MonoBehaviour
+    {
+        [SerializeField] AudioClip _damagedSound = null;
+        [SerializeField] AudioClip _killedSound = null;
+        [SerializeField] Transform _locationToPlay = null;
+
+        Health _health = null;
+
+        private void Awake()
+        {
+            _health = GetComponent<Health>();
+        }
+
+        private void OnEnable()
+        {
+            _health.Damaged += OnDamaged;
+            _health.Killed += OnKilled;
+        }
+
+        private void OnDisable()
+        {
+            _health.Damaged -= OnDamaged;
+            _health.Killed -= OnKilled;
+        }
+
+        void OnDamaged(int damage)
+        {
+            if(_damagedSound != null && _locationToPlay != null)
+            {
+                AudioSource.PlayClipAtPoint
+                    (_damagedSound, _locationToPlay.position);
+            }
+        }
+
+        void OnKilled()
+        {
+            if (_damagedSound != null && _locationToPlay != null)
+            {
+                AudioSource.PlayClipAtPoint
+                    (_killedSound, _locationToPlay.position);
+            }
+        }
+    }
+}
+
